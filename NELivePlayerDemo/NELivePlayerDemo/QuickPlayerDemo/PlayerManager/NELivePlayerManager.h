06fb3b0cc2dbb0bca585a9876d6dfc563f817868
youlianqi <youlianqi@corp.netease.com>
2024-07-02 09:59:33 +0800

    perf: 添加注释

--- NELivePlayerDemo/NELivePlayerDemo/QuickPlayerDemo/PlayerManager/NELivePlayerManager.h
+++ NELivePlayerDemo/NELivePlayerDemo/QuickPlayerDemo/PlayerManager/NELivePlayerManager.h
@@ -64,6 +64,9 @@ extern NSString *const NELivePlayerPlayViewNotAddSubviewNotification;
 */
 - (BOOL)setLivePlayerUrlList:(NSArray<NSString *> *)prepareUrlList playingUrl:(NSString *)playingUrl nextPlayUrl:(NSString *)nextPlayUrl;
 
+/**
+ * 切换当前页面播放器的url
+ */
 - (BOOL)switchPlayingUrl:(NSString *)playingUrl;
 /**
  * 根据播放器地址获取 NELivePlayer
